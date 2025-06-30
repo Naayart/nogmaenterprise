1e981536cee416a1c562e1c162f184b13a630b0c
Naayart <jerusha328@gmail.com>
2025-06-30 09:18:10 -0700

    addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
-export default function Navbar(){
-    return(
-        <>
-        <h1>Hello</h1>
-        </>
-    );
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <h1>
+          <span>Nogma</span>
+          <span>H</span>
+          <span>Enterprise</span>
+        </h1>
+
+        <div className=" space-x-5 inline-block ">
+          <button className="font-bold ">Login</button>
+          <button className="bg-[#7848F4] text-white px-7 py-2 rounded-sm">
+            Signup
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 }
